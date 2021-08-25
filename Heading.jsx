124b1dae5bc5fc7e51c565cd54181621a3dd7696
Md Adil Alam <31787784+Adilalaminfo@users.noreply.github.com>
2021-08-25 17:04:30 +0530

    Add files via upload

---
+++ Heading.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+ 
+function Heading(){
+
+return <h1>My Name is Md Adil Alam</h1>
+}
+export default Heading;
---
+++ List.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function List(){
+    return (
+
+   <ol>
+       <li>Adil</li>
+       <li>Saddam</li>
+       <li>Hasre</li>
+       <li>Ejaz</li>
+       <li>Saheb</li>
+       <li>Furquan</li>
+   </ol>
+    );
+}
+export default List;
